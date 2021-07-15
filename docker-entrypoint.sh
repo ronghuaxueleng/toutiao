413a4e21af0e26ef3a6c1ee06a3586666b2e7272
caoqiang <Xinyan1203>
2021-07-15 15:44:58 +0800

    up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,6 +28,6 @@ bash git_pull
 echo
 
 UseNotes
-/usr/bin/mitmdump -s mproxy.py --set block_global=false --mode socks5 &
-/usr/local/shadowsocks/server.py -c /etc/shadowsocks-r/config.json
+/usr/local/shadowsocks/server.py -c /etc/shadowsocks-r/config.json &
+/usr/bin/mitmdump -s mproxy.py --set block_global=false --mode socks5
 exec "$@"
