d5548604714c54232358d51a58e3e0e0c41f13f5
ronghuaxueleng <caoqianghappy@126.com>
2022-06-23 09:10:16 +0800

    up

--- others/xiaomistep.py
+++ others/xiaomistep.py
@@ -8,3 +8,12 @@ headers = {}
 response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
+
+url = "https://api.kit9.cn/api/milletmotion/?mobile=15901296902&password=sxy116816&step=100000"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
