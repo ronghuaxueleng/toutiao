bb928a04d78c908a6c854ade1edef494e7ef3e29
qiangcao <caoqianghappy@126.com>
2022-06-28 22:28:18 +0800

    Update xiaomistep.py
    修改

--- others/xiaomistep.py
+++ others/xiaomistep.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.kit9.cn/api/milletmotion/?mobile=15901254680&password=xinyan1203&step=49999"
+url = "https://api.kit9.cn/api/milletmotion/?mobile=15901254680&password=xinyan1203&step=47000"
 
 payload={}
 headers = {}
@@ -9,7 +9,7 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
 
-url = "https://api.kit9.cn/api/milletmotion/?mobile=15901296902&password=sxy116816&step=49999"
+url = "https://api.kit9.cn/api/milletmotion/?mobile=15901296902&password=sxy116816&step=47000"
 
 payload={}
 headers = {}
