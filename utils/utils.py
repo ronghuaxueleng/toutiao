1d922f59ad973aa502600d99cea1de7cb5ca2a94
ronghuaxueleng <caoqianghappy@126.com>
2022-07-27 17:51:02 +0800

    httpx

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,7 @@ import httpx
 
 import requests
 from chinese_calendar import is_workday
-# from hyper import HTTP20Connection
+from hyper import HTTP20Connection
 
 
 def get_sample(url, headers):
@@ -17,31 +17,23 @@ def post_sample(url, headers, data=None):
     return httpx.post(url, headers=headers, data=data)
 
 
-def get(url, headers):
-    return httpx.get(url, headers=headers)
+def get(host, path, headers):
+    return request(host, 'GET', path, headers)
 
 
-def post(url, headers, data=None):
-    return httpx.post(url, headers=headers, data=data)
+def post(host, path, headers, data=None):
+    return request(host, 'POST', path, headers, data)
 
 
-# def get(host, path, headers):
-#     return request(host, 'GET', path, headers)
-#
-#
-# def post(host, path, headers, data=None):
-#     return request(host, 'POST', path, headers, data)
-
-
-# def request(host, method, path, headers=None, body=None):
-#     if headers is None:
-#         headers = {}
-#     headers['Accept'] = '*/*'
-#     headers['Connection'] = 'keep-alive'
-#     c = HTTP20Connection(host)
-#     response = c.request(method, path, headers=headers, body=body)
-#     resp = c.get_response(response)
-#     return resp.read().decode('utf-8')
+def request(host, method, path, headers=None, body=None):
+    if headers is None:
+        headers = {}
+    headers['Accept'] = '*/*'
+    headers['Connection'] = 'keep-alive'
+    c = HTTP20Connection(host)
+    response = c.request(method, path, headers=headers, body=body)
+    resp = c.get_response(response)
+    return resp.read().decode('utf-8')
 
 
 def convert_cookies_to_dict(cookies, delimiter="; |;|, |,"):
