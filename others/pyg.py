24caa7b07e833922072dacb18fb2d9058d244ca1
ronghuaxueleng <caoqianghappy@126.com>
2022-08-11 09:38:56 +0800

    飘云阁更新

--- others/pyg.py
+++ others/pyg.py
@@ -1,8 +1,10 @@
 import requests
 
-url = "https://www.chinapyg.com/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&sign_as=1&inajax=1"
+session = requests.session()
 
-payload='formhash=318e4f52&qdxq=kx&qdmode=2&todaysay=&fastreply=0'
+url = "https://www.chinapyg.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1"
+
+payload='fastloginfield=email&username=caoqianghappy%40126.com&password=3f0ed655a128fedda9efd124afbe3037&quickforward=yes'
 headers = {
   'Connection': 'keep-alive',
   'Cache-Control': 'max-age=0',
@@ -20,9 +22,17 @@ headers = {
   'Sec-Fetch-Dest': 'iframe',
   'Referer': 'https://www.chinapyg.com/',
   'Accept-Language': 'zh-CN,zh;q=0.9',
-  'Cookie': 'discuz_2132_saltkey=vdD0dj40; discuz_2132_lastvisit=1657457842; discuz_2132_auth=8f8fYYgwDcBbki%2Bnl2iAI3rjX6RqeDCWaZuK3pQuBqWsaeQTrhwCK49BluvNuvXRRJsT9n8%2FIqdmZX2lKUIDRZPYKQ; discuz_2132_connect_is_bind=0; discuz_2132_nofavfid=1; discuz_2132_smile=1D1; discuz_2132_st_p=62025%7C1659488656%7C4d63ea4661c3b0fb65fa02aab91ba990; discuz_2132_viewid=tid_144234; discuz_2132_sid=nxS227; discuz_2132_lip=101.40.69.178%2C1659488650; discuz_2132_onlineusernum=2151; discuz_2132_ulastactivity=6be0TS4GvcGrjEHSq0G8Y98A%2Bvha58BF42gVHZ9g46ZN1JVsY9Au; discuz_2132_lastcheckfeed=62025%7C1659574875; discuz_2132_checkfollow=1; discuz_2132_checkpm=1; discuz_2132_sendmail=1; discuz_2132_lastact=1659574891%09plugin.php%09; discuz_2132_connect_is_bind=0; discuz_2132_lastact=1659574998%09plugin.php%09'
+  'Cookie': 'discuz_2132_auth=b1fa%2FGIxoucSsq8%2BRh3dwqdh%2Fvmi8mtKAe4LOUkH7ZIobR4jO7WYV%2Bb27s9aH0akSly6iNadoyPZ302wfLHnfqTRUQ; discuz_2132_connect_is_bind=0; discuz_2132_lastact=1660098953%09member.php%09logging; discuz_2132_lastcheckfeed=62025%7C1660098932; discuz_2132_lastvisit=1660095332; discuz_2132_lip=101.40.69.178%2C1660098690; discuz_2132_saltkey=r2M4BF9e; discuz_2132_sid=br04W5; discuz_2132_ulastactivity=c871yJ3NJ%2BU%2BFiLbw7vgW%2B7PAdEDO5PeB6XnJhn7vypT1k2gf862'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response = session.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
+
+url = "https://www.chinapyg.com/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&sign_as=1&inajax=1"
+
+payload='qdxq=kx&qdmode=2&todaysay=&fastreply=0'
+response = session.request("POST", url, headers=headers, data=payload)
 
 print(response.text)
