feb723ba6363fc1c84333aa08c22684309e5b9fb
ronghuaxueleng <caoqianghappy@126.com>
2024-02-05 21:34:12 +0800

    哩吥哩吥

---
+++ liblibart/UserInfo.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import json
+import time
+
+import requests
+
+
+class UserInfo(object):
+    def __init__(self, token):
+        self.headers = {
+            'authority': 'liblib-api.vibrou.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'content-length': '0',
+            'content-type': 'application/x-www-form-urlencoded',
+            'dnt': '1',
+            'origin': 'https://www.liblib.art',
+            'sec-ch-ua': '"Chromium";v="119", "Not?A_Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'token': token,
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.160 Safari/537.36',
+            'webid': '1701652270086cvpnqgrl'
+        }
+        self.token = token
+        self.userInfo = {}
+        self.getUserInfo()
+
+    def getUserInfo(self):
+        url = f"https://liblib-api.vibrou.com/api/www/user/getUserInfo?timestamp={time.time()}"
+        payload = {}
+        response = requests.request("POST", url, headers=self.headers, data=payload)
+        self.userInfo = json.loads(response.text)['data']
+
+
+if __name__ == '__main__':
+    token = 'd1894681b7c5438b9051b840431e9b59'
+    userInfo = UserInfo(token)
+    print(userInfo.userInfo)
