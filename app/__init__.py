b01d0b97f6a22f361eeb379e5efb966635f57d96
ronghuaxueleng <caoqianghappy@126.com>
2024-02-24 13:33:34 +0800

    哩吥哩吥

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,12 @@ from urllib.parse import urlencode
 
 from flask import Flask, jsonify
 from flask import render_template
+from peewee import fn
 
-from db.toutiao_jisu import Account, CommonParams
 from run_task import profit_detail
+from db.toutiao_jisu import Account, CommonParams
+from liblibart.UserInfo import Account as LiblibAccount
+from liblibart.Statistics import DownLoadImageStatistics, DownloadModelStatistics, RunStatistics
 
 app = Flask(__name__)
 
@@ -27,4 +30,29 @@ def getToutiao():
         query = urlencode(params)
         result = profit_detail(headers, query, account, True)
         results.append(result)
+    return jsonify(results)
+
+
+@app.route('/getLiblib')
+def getLiblib():
+    results = []
+    accounts = LiblibAccount.select()
+    for idx, account in enumerate(accounts):
+        nickname = account.nickname
+        user_uuid = account.user_uuid
+        downloadImageCount = fn.SUM(DownLoadImageStatistics.downloadImageCount).alias('downloadImageCount')
+        downLoadImages = DownLoadImageStatistics.select(downloadImageCount).where(DownLoadImageStatistics.user_uuid == user_uuid).get()
+        downloadImageCounts = downLoadImages.downloadImageCount
+        downloadModelCount = fn.SUM(DownloadModelStatistics.downloadModelCount).alias('downloadModelCount')
+        downloadModels = DownloadModelStatistics.select(downloadModelCount).where(DownloadModelStatistics.user_uuid == user_uuid).get()
+        downloadModelCounts = downloadModels.downloadModelCount
+        runCount = fn.SUM(RunStatistics.runCount).alias('runCount')
+        runs = RunStatistics.select(runCount).where(RunStatistics.user_uuid == user_uuid).get()
+        runCounts = runs.runCount
+        results.append({
+            'nickname': nickname,
+            'runCounts': 0 if runCounts is None else runCounts,
+            'downloadModelCounts': 0 if downloadModelCounts is None else downloadModelCounts,
+            'downloadImageCounts': 0 if downloadImageCounts is None else downloadImageCounts
+        })
     return jsonify(results)
