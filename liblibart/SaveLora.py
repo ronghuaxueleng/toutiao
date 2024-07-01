0ab19672639c084cc5039b55da85d3a40c3a1b75
caoqiang <caoqiang>
2024-07-01 10:52:15 +0800

    liblib

--- liblibart/SaveLora.py
+++ liblibart/SaveLora.py
@@ -13,15 +13,7 @@ class SaveLora(UserInfo):
     def __init__(self, token, webid, log_filename):
         super().__init__(token, webid, log_filename)
 
-    def get_models(self, pageNo):
-        url = f"https://{self.api_host}/api/www/model/list?timestamp={time.time()}"
-        payload = json.dumps({
-            "pageNo": pageNo,
-            "pageSize": 20,
-            "uuid": self.userInfo['uuid'],
-            "status": -2,
-            "type": 0
-        })
+    def get_models(self):
         headers = {
             'authority': self.api_host,
             'accept': 'application/json, text/plain, */*',
@@ -41,8 +33,6 @@ class SaveLora(UserInfo):
             'webid': self.webid
         }
 
-        response = requests.request("POST", url, headers=headers, data=payload)
-
         my_loras = ql_env.search("my_lora")
         saved_models = []
         saved_model_id_map = {}
@@ -53,35 +43,47 @@ class SaveLora(UserInfo):
                 saved_models.append(modelId)
                 saved_model_id_map[modelId] = my_lora['id']
 
-        data = json.loads(response.text)
-        for model in data['data']['list']:
-            version_url = f"https://{self.api_host}/api/www/model/getByUuid/{model['uuid']}?timestamp={time.time()}"
-            payload = {}
-            response = requests.request("POST", version_url, headers=headers, data=payload)
+        for pageNo in range(1, 5):
+            url = f"https://{self.api_host}/api/www/model/list?timestamp={time.time()}"
+            payload = json.dumps({
+                "pageNo": pageNo,
+                "pageSize": 20,
+                "uuid": self.userInfo['uuid'],
+                "status": -2,
+                "type": 0
+            })
 
-            model_data = json.loads(response.text)
-            for version in model_data['data']['versions']:
-                to_save_data = {
-                    "modelId": version["id"],
-                    "type": 0,
-                    "modelName": model["name"],
-                    "modelVersionName": version['name'],
-                    "weight": 0.8,
-                    "userUuid": self.userInfo['uuid'],
-                    "modelType": model['modelType']
-                }
-                if version['id'] not in saved_models:
-                    ql_env.add("my_lora", json.dumps(to_save_data, ensure_ascii=False), model["name"])
-                else:
-                    ql_env.update(json.dumps(to_save_data, ensure_ascii=False), model["name"], saved_model_id_map[version['id']])
+            response = requests.request("POST", url, headers=headers, data=payload)
+            data = json.loads(response.text)
+            if len(data['data']['list']) == 0:
+                break
+            for model in data['data']['list']:
+                version_url = f"https://{self.api_host}/api/www/model/getByUuid/{model['uuid']}?timestamp={time.time()}"
+                payload = {}
+                response = requests.request("POST", version_url, headers=headers, data=payload)
+
+                model_data = json.loads(response.text)
+                for version in model_data['data']['versions']:
+                    to_save_data = {
+                        "modelId": version["id"],
+                        "type": 0,
+                        "modelName": model["name"],
+                        "modelVersionName": version['name'],
+                        "weight": 0.8,
+                        "userUuid": self.userInfo['uuid'],
+                        "modelType": model['modelType']
+                    }
+                    if version['id'] not in saved_models:
+                        ql_env.add("my_lora", json.dumps(to_save_data, ensure_ascii=False), model["name"])
+                    else:
+                        ql_env.update(json.dumps(to_save_data, ensure_ascii=False), "my_lora", saved_model_id_map[version['id']], model["name"])
 
 
 if __name__ == '__main__':
-    for pageNo in range(1, 5):
-        users = get_users()
-        for user in users:
-            try:
-                SaveLora(user['usertoken'], user['webid'], '/mitmproxy/logs/SaveLora.log').get_models(pageNo)
-            except Exception as e:
-                print('error', e)
-                print(e)
+    users = get_users()
+    for user in users:
+        try:
+            SaveLora(user['usertoken'], user['webid'], '/mitmproxy/logs/SaveLora.log').get_models()
+        except Exception as e:
+            print('error', e)
+            print(e)
