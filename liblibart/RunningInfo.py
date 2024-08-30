4875932f95a06cd5ea718d21e048b92c6c12dae8
ronghuaxueleng <caoqianghappy@126.com>
2024-08-30 21:07:50 +0800

    mysql

---
+++ liblibart/RunningInfo.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from peewee import *
+
+from liblibart.DbUtils import get_conn
+
+db = get_conn()
+
+
+# 运行信息
+class RunningInfo(Model):
+    _id = PrimaryKeyField
+    to_run_users = TextField(null=True)
+    checkpointIds = TextField(null=True)
+    current_day = CharField(null=True)
+    suanlibuzu_users = TextField(null=True)
+    timestamp = DateTimeField(null=True, default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+if __name__ == '__main__':
+    db.connect()
+    db.create_tables([RunningInfo])
