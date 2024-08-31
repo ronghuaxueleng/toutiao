9c834b73fe29160110a70b868b43b7230f3f338c
ronghuaxueleng <caoqianghappy@126.com>
2024-08-31 17:36:02 +0800

    保存lora

---
+++ liblibart/Model.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from peewee import *
+
+from liblibart.DbUtils import get_conn
+
+db = get_conn()
+
+
+class Model(Model):
+    _id = PrimaryKeyField
+    user_uuid = CharField(null=False)
+    user_name = CharField(null=False)
+    modelId = CharField(null=False)
+    modelName = CharField(null=False)
+    modelVersionName = CharField(null=False)
+    modelType = IntegerField(null=False)
+    showType = IntegerField(null=False)
+    createTime = DateTimeField(null=True)
+    updateTime = DateTimeField(null=True)
+    timestamp = DateTimeField(null=True, default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+def create_tables(tables: list):
+    db.connect()
+    for table in tables:
+        u"""
+        如果table不存在，新建table
+        """
+        if not table.table_exists():
+            table.create_table()
+
+
+if __name__ == '__main__':
+    create_tables([Model])
