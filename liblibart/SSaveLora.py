968d05f07b9634e4ce859bcabf6c77a44aa9b889
caoqiang <caoqiang>
2024-09-02 14:44:27 +0800

    添加stask

---
+++ liblibart/SSaveLora.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+import datetime
+import json
+import os
+import time
+
+import requests
+
+from CookieUtils import get_users
+from Model import Model
+from SUserInfo import SUserInfo
+from ql import ql_env
+
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+
+# 指定env文件
+env_path = Path.cwd().joinpath('env').joinpath('sos.env')
+env_path.parent.mkdir(exist_ok=True)
+load_dotenv(find_dotenv(str(env_path)))
+
+
+class SSaveLora(SUserInfo):
+    def __init__(self, token, webid, bl_uid, log_filename):
+        super().__init__(token, webid, bl_uid, log_filename)
+
+    def get_models(self):
+        headers = {
+            'authority': self.api_host,
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'content-type': 'application/json',
+            'dnt': '1',
+            'origin': f'https://{self.web_host}',
+            'referer': f"https://{self.web_host}/userpage/{self.userInfo['uuid']}/publish",
+            'sec-ch-ua': '"Chromium";v="119", "Not?A_Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'token': self.token,
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.160 Safari/537.36',
+            'webid': self.webid
+        }
+
+        saved_models = []
+        saved_model_id_map = {}
+        try:
+            my_loras = ql_env.search("my_lora")
+            for my_lora in my_loras:
+                if my_lora['status'] == 0:
+                    value = json.loads(my_lora['value'])
+                    modelId = value['modelId']
+                    saved_models.append(modelId)
+                    saved_model_id_map[modelId] = my_lora['id']
+        except Exception as e:
+            print(e)
+
+        pageNo = 1
+        total_models = None
+        while total_models is None or total_models > 0:
+            url = f"https://{self.api_host}/api/www/model/list?timestamp={time.time()}"
+            payload = json.dumps({
+                "pageNo": pageNo,
+                "pageSize": 10,
+                "status": -2,
+                "type": 0
+            })
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+            data = json.loads(response.text)
+            pageNo = pageNo + 1
+            total_models = len(data['data']['list'])
+            for model in data['data']['list']:
+                version_url = f"https://{self.api_host}/api/www/model/getByUuid/{model['uuid']}?timestamp={time.time()}"
+                payload = {}
+                response = requests.request("POST", version_url, headers=headers, data=payload)
+
+                model_data = json.loads(response.text)
+                for version in model_data['data']['versions']:
+                    to_save_data = {
+                        "modelId": version["id"],
+                        "type": 0,
+                        "modelName": model["name"],
+                        "modelVersionName": version['name'],
+                        "weight": 0.8,
+                        "userUuid": self.userInfo['uuid'],
+                        "modelType": model['modelType']
+                    }
+                    if version['id'] not in saved_models:
+                        ql_env.add("my_lora", json.dumps(to_save_data, ensure_ascii=False), model["name"])
+                    else:
+                        ql_env.update(json.dumps(to_save_data, ensure_ascii=False), "my_lora",
+                                      saved_model_id_map[version['id']], model["name"])
+
+                    query = Model.select().where(Model.user_uuid == self.userInfo['uuid'],
+                                                 Model.modelId == version["id"])
+                    if query.exists():
+                        Model.update(
+                            user_name=self.userInfo['nickname'],
+                            modelName=model["name"],
+                            modelVersionName=version['name'],
+                            showType=version['showType'],
+                            updateTime=version['updateTime'],
+                            timestamp=datetime.datetime.now()
+                        ).where(Model.user_uuid == self.userInfo['uuid'], Model.modelId == version["id"]).execute()
+                    else:
+                        Model.insert(
+                            user_uuid=self.userInfo['uuid'],
+                            user_name=self.userInfo['nickname'],
+                            modelId=version["id"],
+                            modelName=model["name"],
+                            modelVersionName=version['name'],
+                            modelType=model['modelType'],
+                            showType=version['showType'],
+                            createTime=version['createTime'],
+                            updateTime=version['updateTime'],
+                        ).execute()
+
+
+if __name__ == '__main__':
+    users = get_users()
+    for user in users:
+        try:
+            SaveLora(user['usertoken'], user['webid'],
+                     f'/mitmproxy/logs/SaveLora_{os.getenv("RUN_OS_KEY")}.log').get_models()
+        except Exception as e:
+            print('error', e)
+            print(e)
---
+++ liblibart/Stasks.py
@@ -0,0 +1,362 @@
+import copy
+import datetime
+import json
+import os
+import random
+import time
+from pathlib import Path
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from CookieUtils import get_users, load_from_run_users, save_to_run_users, load_from_suanlibuzu_users
+from DownLoadImage import DownLoadImage
+from DownloadModel import DownloadModel
+from Image import Image
+from UserInfo import UserInfo, Account
+from ql import ql_env
+from utils import send_message
+
+scheduler = BlockingScheduler(timezone='Asia/Shanghai')
+
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+
+# 指定env文件
+env_path = Path.cwd().joinpath('env').joinpath('sos.env')
+print(env_path)
+env_path.parent.mkdir(exist_ok=True)
+load_dotenv(find_dotenv(str(env_path)))
+
+misfire_grace_time = 60
+
+
+class SLiblibTasks:
+    def __init__(self):
+        self.yesterday = 0
+        self.today = 0
+        self.notAvailableToImageUsers = {}
+        self.notAvailableImageUsersFileName = 'snotAvailableToImageUsers.json'
+
+    def init_day(self):
+        dt = datetime.datetime.now()
+        self.yesterday = (dt - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        self.today = dt.strftime('%Y-%m-%d')
+
+    def init_tasks(self):
+        scheduler.add_job(
+            self.downloadModel,
+            id='downloadModel',
+            trigger='date',
+            misfire_grace_time=misfire_grace_time,
+            run_date=self.get_download_model_run_date(),
+        )
+
+        scheduler.add_job(
+            self.downLoadImage,
+            id='downLoadImage',
+            trigger='date',
+            misfire_grace_time=misfire_grace_time,
+            run_date=self.get_downLoad_image_run_date(),
+        )
+
+        scheduler.add_job(
+            self.drawImage,
+            id='drawImage',
+            trigger='date',
+            misfire_grace_time=misfire_grace_time,
+            run_date=self.get_draw_image_run_date(),
+        )
+        self.get_all_job()
+        return self
+
+    def init(self):
+        self.drawImage()
+        self.downloadModel()
+        self.downLoadImage()
+        return self
+
+    def start(self):
+        scheduler.start()
+        return self
+
+    def get_all_job(self, message=None):
+        all_jobs = scheduler.get_jobs()
+        print(all_jobs)
+        msg = []
+        if message is not None:
+            msg.append(message)
+        for job in all_jobs:
+            msg.append(f'任务ID：{job.id}，执行时间：{job.trigger}')
+        send_message("\n".join(msg), title=f'shakker-{os.getenv("RUN_OS_NAME")}')
+
+    def get_models(self):
+        user_model_dict = {}
+        try:
+            my_loras = ql_env.search("my_lora")
+            for my_lora in my_loras:
+                if my_lora['status'] == 0:
+                    value = json.loads(my_lora['value'])
+                    user_models = user_model_dict.setdefault(value['userUuid'], [])
+                    user_models.append(value)
+                    user_model_dict[value['userUuid']] = user_models
+        except Exception as e:
+            print(e)
+
+        final_user_model_dict = {}
+        users = get_users()
+        for user in users:
+            _user_model_dict = copy.deepcopy(user_model_dict)
+            usertoken = user['usertoken']
+            if usertoken in _user_model_dict:
+                del _user_model_dict[usertoken]
+            final_user_model_list = final_user_model_dict.setdefault(usertoken, [])
+            for user_model in _user_model_dict.values():
+                final_user_model_list.extend(user_model)
+            final_user_model_dict[usertoken] = final_user_model_list
+        return final_user_model_dict
+
+    def update_userInfo(self):
+        users = get_users(True)
+        disable_ids = []
+        enable_ids = []
+        for user in users:
+            try:
+                userInfo = UserInfo(user['usertoken'], user['webid'],
+                                    f'/mitmproxy/logs/UserInfo_{os.getenv("RUN_OS_KEY")}.log')
+                realUser = userInfo.userInfo
+                if realUser is not None:
+                    enable_ids.append(user['id'])
+                    uuid = realUser['uuid']
+                    nickname = realUser['nickname']
+                    query = Account.select().where(Account.user_uuid == uuid)
+                    if query.exists():
+                        Account.update(
+                            user_uuid=uuid,
+                            nickname=nickname,
+                            userInfo=json.dumps(realUser)
+                        ).where(Account.user_uuid == uuid).execute()
+                    else:
+                        Account.insert(
+                            user_uuid=uuid,
+                            nickname=nickname,
+                            userInfo=json.dumps(realUser)
+                        ).execute()
+                else:
+                    disable_ids.append(user['id'])
+            except Exception as e:
+                print(e)
+        if len(disable_ids) > 0:
+            ql_env.disable(disable_ids)
+        if len(enable_ids) > 0:
+            ql_env.enable(enable_ids)
+
+    def get_download_model_run_date(self):
+        return datetime.datetime.now() + datetime.timedelta(days=random.randint(1, 3), hours=random.randint(1, 23),
+                                                            minutes=random.randint(0, 59),
+                                                            seconds=random.randint(0, 59))
+
+    def get_downLoad_image_run_date(self):
+        return datetime.datetime.now() + datetime.timedelta(hours=random.randint(3, 5),
+                                                            minutes=random.sample([11, 23, 37, 42, 57],
+                                                                                  1)[0],
+                                                            seconds=random.randint(0, 59))
+
+    def get_draw_image_run_date(self):
+        return datetime.datetime.now() + datetime.timedelta(minutes=random.randint(7, 15),
+                                                            seconds=random.randint(0, 59))
+
+    def downloadModel(self):
+        send_message("开始执行模型下载", title=f'shakker-{os.getenv("RUN_OS_NAME")}')
+        job_id = 'downloadModel'
+        users = get_users()
+        for user in random.sample(users, 4):
+            try:
+                DownloadModel(user['usertoken'], user['webid'],
+                              f'/mitmproxy/logs/DownloadModel_{os.getenv("RUN_OS_KEY")}.log').download_model()
+            except Exception as e:
+                print(e)
+        s = scheduler.get_job(job_id)
+        if s is None:
+            scheduler.add_job(
+                self.downloadModel,
+                id=job_id,
+                trigger='date',
+                misfire_grace_time=misfire_grace_time,
+                run_date=self.get_download_model_run_date(),
+            )
+        else:
+            scheduler.reschedule_job(
+                job_id,
+                run_date=self.get_download_model_run_date()
+            )
+        self.get_all_job('模型下载结束')
+
+    def downLoadImage(self):
+        send_message("开始执行图片下载", title=f'shakker-{os.getenv("RUN_OS_NAME")}')
+        job_id = "downLoadImage"
+
+        users = get_users()
+        for user in users:
+            try:
+                DownLoadImage(user['usertoken'], user['webid'],
+                              f'/mitmproxy/logs/DownLoadImage_{os.getenv("RUN_OS_KEY")}.log').download()
+            except Exception as e:
+                print(e)
+        s = scheduler.get_job(job_id)
+        if s is None:
+            if scheduler.get_job(job_id) is None:
+                scheduler.add_job(
+                    self.downLoadImage,
+                    id=job_id,
+                    trigger='date',
+                    misfire_grace_time=misfire_grace_time,
+                    run_date=self.get_downLoad_image_run_date(),
+                )
+        else:
+            scheduler.reschedule_job(
+                job_id,
+                run_date=self.get_downLoad_image_run_date(),
+            )
+        self.get_all_job('图片下载结束')
+
+    def drawImage(self):
+        self.init_day()
+        star_time = datetime.datetime.now()
+        send_message("开始执行绘图", title=f'shakker-{os.getenv("RUN_OS_NAME")}')
+        job_id = f"drawImage"
+        suanlibuzu = []
+        if os.path.exists(Path.cwd().joinpath(self.notAvailableImageUsersFileName)):
+            with open(Path.cwd().joinpath(self.notAvailableImageUsersFileName), 'r') as f:
+                self.notAvailableToImageUsers = json.load(f)
+        if self.yesterday in self.notAvailableToImageUsers:
+            del self.notAvailableToImageUsers[self.yesterday]
+            with open(Path.cwd().joinpath(self.notAvailableImageUsersFileName), 'w') as f:
+                json.dump(self.notAvailableToImageUsers, f)
+
+        def get_percent(user, image, image_num, depth):
+            res = image.get_percent(image_num)
+            if res['code'] == 0:
+                percentCompleted = res['data']['percentCompleted']
+                image.getLogger().info(f"nickname：{image.userInfo['nickname']}，{percentCompleted}%.....")
+                if percentCompleted != 100:
+                    time.sleep(7)
+                    get_percent(user, image, image_num, depth + 1)
+                else:
+                    image.getLogger().info(f"finished nickname：{image.userInfo['nickname']}，100%.....")
+                    image.nps()
+                    try:
+                        DownLoadImage(user['usertoken'], user['webid'],
+                                      f'/mitmproxy/logs/DownLoadImage_{os.getenv("RUN_OS_KEY")}.log').download(
+                            False)
+                    except Exception as e:
+                        print(e)
+                    image.getLogger().info(f'递归层级{depth}')
+                    return depth
+
+        def doDrawImage(user, my_loras):
+            try:
+                to_save_run_users = load_from_run_users()
+                if user['usertoken'] in to_save_run_users:
+                    to_save_run_users.remove(user['usertoken'])
+                    save_to_run_users(list(set(to_save_run_users)))
+                if user['usertoken'] not in suanlibuzu:
+                    image = Image(user['usertoken'], user['webid'],
+                                  f'/mitmproxy/logs/Image_{os.getenv("RUN_OS_KEY")}.log')
+                    runCount = {}
+                    for model in my_loras:
+                        userUuid = model['userUuid']
+                        del model['userUuid']
+                        del model['modelType']
+                        image.param['additionalNetwork'].append(model)
+                        run_model = runCount.setdefault(userUuid, {})
+                        __model = run_model.setdefault(model['modelId'], model)
+                        run_count = __model.setdefault('count', 0)
+                        runCount[userUuid][model['modelId']]['count'] = run_count + 1
+                    image_num = image.gen(runCount)
+                    if image_num == 'suanlibuzu':
+                        suanlibuzu.append(user['usertoken'])
+                        notAvailableToImageUsers = self.notAvailableToImageUsers.setdefault(self.today, [])
+                        notAvailableToImageUsers.append(user['usertoken'])
+                        self.notAvailableToImageUsers[self.today] = notAvailableToImageUsers
+                        raise Exception('算力不足')
+                    elif image_num == 'kongjianbuzu':
+                        image.getLogger().error('图库空间不足')
+                        raise Exception('图库空间不足')
+                    elif image_num == 'qitacuowu':
+                        raise Exception('报错了')
+                    elif image_num == 'tokenwuxiao':
+                        image.getLogger().error('token无效')
+                        self.update_userInfo()
+                        raise Exception('token无效')
+                    else:
+                        get_percent(user, image, image_num, 1)
+            except Exception as e:
+                print(e)
+
+        exclude_user = self.notAvailableToImageUsers.setdefault(self.today, [])
+        to_run_users = load_from_run_users()
+        exclude_user.extend(to_run_users)
+        suanlibuzu_user = load_from_suanlibuzu_users()
+        exclude_user.extend(suanlibuzu_user)
+        users = get_users(exclude_user=exclude_user)
+        if len(users) == 0:
+            self.notAvailableToImageUsers[self.today] = []
+            save_to_run_users([])
+        user_model_dict = self.get_models()
+
+        def simple_generator():
+            # 当前时间
+            now_localtime = time.strftime("%H:%M:%S", time.localtime())
+            is_time = False  #"00:00:00" < now_localtime < "08:00:00"
+            # to_run_user_count = (10 if len(users) >= 10 else len(users)) if is_time else (
+            #     5 if len(users) >= 5 else len(users))
+            to_run_user_count = 5 if len(users) >= 5 else len(users)
+            to_save_run_users = load_from_run_users()
+            to_run_users = random.sample(users, to_run_user_count)
+            for user in to_run_users:
+                to_save_run_users.append(user['usertoken'])
+            save_to_run_users(list(set(to_save_run_users)))
+            for user in to_run_users:
+                # for user in users:
+                to_run_models = user_model_dict[user['usertoken']]
+                to_run_model_count = (30 if len(to_run_models) >= 30 else len(to_run_models)) if is_time else (
+                    20 if len(to_run_models) >= 20 else len(to_run_models))
+                to_run_models = random.sample(to_run_models, to_run_model_count)
+                group_every_two = [to_run_models[i:i + 1] for i in range(0, len(to_run_models), 1)]
+                for to_run_model in group_every_two:
+                    yield doDrawImage(user, to_run_model)
+
+        gen = simple_generator()
+        try:
+            while True:
+                next(gen)
+        except StopIteration as e:
+            print(e.value)
+        finally:
+            gen.close()
+            s = scheduler.get_job(job_id)
+            if s is None:
+                scheduler.add_job(
+                    self.drawImage,
+                    id=job_id,
+                    trigger='date',
+                    misfire_grace_time=misfire_grace_time,
+                    run_date=self.get_draw_image_run_date(),
+                )
+            else:
+                scheduler.reschedule_job(
+                    job_id,
+                    run_date=self.get_draw_image_run_date(),
+                )
+            with open(Path.cwd().joinpath(self.notAvailableImageUsersFileName), 'w') as f:
+                json.dump(self.notAvailableToImageUsers, f)
+            end_time = datetime.datetime.now()
+            time_consuming = (end_time - star_time).seconds / 60
+            self.get_all_job(f'绘图结束\n耗时{time_consuming}分')
+
+
+sLiblibTasks = SLiblibTasks()
+
+if __name__ == '__main__':
+    sLiblibTasks.drawImage()
+    # liblibTasks.init_tasks()
