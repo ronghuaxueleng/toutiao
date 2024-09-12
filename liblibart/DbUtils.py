4d377e15672e81ee135611b07e849aa569c48843
caoqiang <caoqiang>
2024-09-12 09:44:24 +0800

    使用数据库连接池
    (cherry picked from commit d0417e6e5cbd71b4298d007630a9d651f7791226)

--- liblibart/DbUtils.py
+++ liblibart/DbUtils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import base64
 
-from peewee import MySQLDatabase
+from playhouse.pool import PooledMySQLDatabase
 
 
 def get_conn(database='bGlibGli'):
@@ -9,9 +9,11 @@ def get_conn(database='bGlibGli'):
     password = 'MTIzNDU2Nzg='
     host = 'MTkyLjE0NC4yMTUuMjE4'
     port = 'MzMwNjA='
-    return MySQLDatabase(str(base64.b64decode(database), 'utf-8'),
+    return PooledMySQLDatabase(str(base64.b64decode(database), 'utf-8'),
                          user=str(base64.b64decode(user), 'utf-8'),
                          password=str(base64.b64decode(password), 'utf-8'),
                          host=str(base64.b64decode(host), 'utf-8'),
-                         port=int(str(base64.b64decode(port), 'utf-8'))
+                         port=int(str(base64.b64decode(port), 'utf-8')),
+                        max_connections=10,
+                        stale_timeout=300,
                          )
