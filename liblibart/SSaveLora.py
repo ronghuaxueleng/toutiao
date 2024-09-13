17b8010231666dd5102aef77b0ebd99caa6095d8
caoqiang <caoqiang>
2024-09-13 10:15:26 +0800

    模型保存

--- liblibart/SSaveLora.py
+++ liblibart/SSaveLora.py
@@ -24,27 +24,14 @@ class SSaveLora(SUserInfo):
     def __init__(self, token, webid, bl_uid, log_filename):
         super().__init__(token, webid, bl_uid, log_filename)
 
-    def get_models(self):
-        headers = {
-            'authority': self.api_host,
-            'accept': 'application/json, text/plain, */*',
-            'accept-language': 'zh-CN,zh;q=0.9',
-            'content-type': 'application/json',
-            'cookie': f'webid={self.webid}; liblibai_usertoken={self.token}; _bl_uid={self.bl_uid}',
-            'dnt': '1',
-            'origin': f'https://{self.web_host}',
-            'referer': f"https://{self.web_host}/userpage/{self.userInfo['uuid']}/publish",
-            'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'token': self.token,
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.95 Safari/537.36',
-            'webid': self.webid,
-            'x-language': 'zh-TW'
-        }
+    def get_models(self, saved_models=None):
+        if saved_models is None:
+            saved_models = {}
+        headers = self.headers
+        headers['content-type'] = 'application/json'
+        headers['referer'] = f"https://{self.web_host}/userpage/{self.userInfo['uuid']}/publish"
+
+        __saved_models = saved_models[self.userInfo['uuid']]
 
         pageNo = 1
         total_models = None
@@ -92,10 +79,9 @@ class SSaveLora(SUserInfo):
                         "remark": "",
                         "ngPrompt": "nsfw,EasyNegative, EasyNegativeV2, ng_deepnegative_v1_75t, worst quality, low quality,bad-hands-5,BadHandsV5"
                     }
-                    query = Model.select().where(
-                        Model.user_uuid == self.userInfo['uuid'],
-                        Model.modelId == version["id"])
-                    if query.exists():
+                    key = f"{self.userInfo['uuid']}_{version['id']}"
+                    if key in __saved_models:
+                        del __saved_models[key]
                         Model.update(
                             user_name=self.userInfo['nickname'],
                             modelName=model["name"],
@@ -121,13 +107,22 @@ class SSaveLora(SUserInfo):
                             updateTime=version['updateTime'],
                         ).execute()
 
+        for model in __saved_models:
+            Model.delete().where(Model.modelId == model.modelId).execute()
 
 if __name__ == '__main__':
+    models = Model.select()
+    saved_models = {}
+    for model in models:
+        __model = saved_models.setdefault(model.user_uuid, {})
+        key = f'{model.user_uuid}_{model.modelId}'
+        __model[key] = model
+        saved_models[model.user_uuid] = __model
     users = get_users(cookie_name="shakker_cookie", usertoken_name="liblibai_usertoken")
     for user in users:
         try:
             SSaveLora(user['usertoken'], user['webid'], user['_bl_uid'],
-                      f'/mitmproxy/logs/SSaveLora_{os.getenv("RUN_OS_KEY")}.log').get_models()
+                      f'/mitmproxy/logs/SSaveLora_{os.getenv("RUN_OS_KEY")}.log').get_models(saved_models)
         except Exception as e:
             print('error', e)
             print(traceback.format_exc())
