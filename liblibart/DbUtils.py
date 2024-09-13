7438f4804c3907a5f81eb2a170c91faa01ee260c
caoqiang <caoqiang>
2024-09-13 13:30:37 +0800

    添加redis链接
    (cherry picked from commit 058c6179ef455c4517325ddedacbaa2c79c9812e)

--- liblibart/DbUtils.py
+++ liblibart/DbUtils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import base64
 
+import redis
 from playhouse.pool import PooledMySQLDatabase
 
 
@@ -10,10 +11,17 @@ def get_conn(database='bGlibGli'):
     host = 'MTkyLjE0NC4yMTUuMjE4'
     port = 'MzMwNjA='
     return PooledMySQLDatabase(str(base64.b64decode(database), 'utf-8'),
-                         user=str(base64.b64decode(user), 'utf-8'),
-                         password=str(base64.b64decode(password), 'utf-8'),
-                         host=str(base64.b64decode(host), 'utf-8'),
-                         port=int(str(base64.b64decode(port), 'utf-8')),
-                        max_connections=20,
-                        stale_timeout=300,
-                         )
+                               user=str(base64.b64decode(user), 'utf-8'),
+                               password=str(base64.b64decode(password), 'utf-8'),
+                               host=str(base64.b64decode(host), 'utf-8'),
+                               port=int(str(base64.b64decode(port), 'utf-8')),
+                               max_connections=20,
+                               stale_timeout=300,
+                               )
+
+
+def get_redis_conn():
+    host = 'MTkyLjE0NC4yMTUuMjE4'
+    port = 'NjM3OQ=='
+    pool = redis.ConnectionPool(host=host, port=port, db=1, decode_responses=True)
+    return redis.Redis(connection_pool=pool)
