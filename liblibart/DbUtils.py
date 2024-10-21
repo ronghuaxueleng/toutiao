da52564735fa2ebfb2894ba6ecec8d53a7140ac8
caoqiang <caoqiang>
2024-10-21 10:58:48 +0800

    数据库断线重连支持
    (cherry picked from commit 970751cff543c9163b71f8f4ede04614b53ef37b)

--- liblibart/DbUtils.py
+++ liblibart/DbUtils.py
@@ -3,21 +3,44 @@ import base64
 
 import redis
 from playhouse.pool import PooledMySQLDatabase
+from playhouse.shortcuts import ReconnectMixin
+
+
+# 断线重连+连接池
+class ReconnectPooledMySQLDatabase(ReconnectMixin, PooledMySQLDatabase):
+    _instance = None
+
+    @classmethod
+    def get_db_instance(cls, database='bGlibGli'):
+        if not cls._instance:
+            user = 'cm9vdA=='
+            password = 'MTIzNDU2Nzg='
+            host = 'MTkyLjE0NC4yMTUuMjE4'
+            port = 'MzMwNjA='
+            cls._instance = cls(str(base64.b64decode(database), 'utf-8'),
+                                user=str(base64.b64decode(user), 'utf-8'),
+                                password=str(base64.b64decode(password), 'utf-8'),
+                                host=str(base64.b64decode(host), 'utf-8'),
+                                port=int(str(base64.b64decode(port), 'utf-8')),
+                                max_connections=10000,
+                                stale_timeout=300,)
+        return cls._instance
 
 
 def get_conn(database='bGlibGli'):
-    user = 'cm9vdA=='
-    password = 'MTIzNDU2Nzg='
-    host = 'MTkyLjE0NC4yMTUuMjE4'
-    port = 'MzMwNjA='
-    return PooledMySQLDatabase(str(base64.b64decode(database), 'utf-8'),
-                               user=str(base64.b64decode(user), 'utf-8'),
-                               password=str(base64.b64decode(password), 'utf-8'),
-                               host=str(base64.b64decode(host), 'utf-8'),
-                               port=int(str(base64.b64decode(port), 'utf-8')),
-                               max_connections=10000,
-                               stale_timeout=300,
-                               )
+    # user = 'cm9vdA=='
+    # password = 'MTIzNDU2Nzg='
+    # host = 'MTkyLjE0NC4yMTUuMjE4'
+    # port = 'MzMwNjA='
+    # return PooledMySQLDatabase(str(base64.b64decode(database), 'utf-8'),
+    #                            user=str(base64.b64decode(user), 'utf-8'),
+    #                            password=str(base64.b64decode(password), 'utf-8'),
+    #                            host=str(base64.b64decode(host), 'utf-8'),
+    #                            port=int(str(base64.b64decode(port), 'utf-8')),
+    #                            max_connections=10000,
+    #                            stale_timeout=300,
+    #                            )
+    return ReconnectPooledMySQLDatabase.get_db_instance(database)
 
 
 def get_redis_conn():
