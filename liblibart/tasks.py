9bcefc5e7edc248857d0de0d43a69da43f0bf7da
caoqiang <caoqiang>
2024-10-21 11:21:43 +0800

    tasks.log打印配置

--- liblibart/tasks.py
+++ liblibart/tasks.py
@@ -6,6 +6,7 @@ import os
 import random
 import time
 import traceback
+from logging import handlers
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -31,11 +32,26 @@ env_path.parent.mkdir(exist_ok=True)
 load_dotenv(find_dotenv(str(env_path)))
 misfire_grace_time = 60
 
-logging.basicConfig(level=logging.ERROR,
-                    filename='/mitmproxy/logs/tasks.log',
-                    datefmt='%Y/%m/%d %H:%M:%S',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(module)s - %(message)s')
 logger = logging.getLogger(__name__)
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S %p"
+format_str = logging.Formatter(LOG_FORMAT, DATE_FORMAT)  #设置日志格式
+logger.setLevel(logging.ERROR)  #设置日志级别
+sh = logging.StreamHandler()  #往屏幕上输出
+sh.setFormatter(format_str)  #设置屏幕上显示的格式
+th = handlers.TimedRotatingFileHandler(filename='/mitmproxy/logs/tasks.log', backupCount=1, encoding='utf-8')
+#往文件里写入#指定间隔时间自动生成文件的处理器
+#实例化TimedRotatingFileHandler
+#interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+# S 秒
+# M 分
+# H 小时、
+# D 天、
+# W 每星期（interval==0时代表星期一）
+# midnight 每天凌晨
+th.setFormatter(format_str)  #设置文件里写入的格式
+logger.addHandler(sh)  #把对象加到logger里
+logger.addHandler(th)
 
 
 class LiblibTasks:
