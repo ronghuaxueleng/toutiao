1b3a0f7f548c058038b14c35f2cc044761f5b057
caoqiang <caoqiang>
2024-11-01 16:52:40 +0800

    修改日志备份文件后缀

--- liblibart/LogInfo.py
+++ liblibart/LogInfo.py
@@ -30,10 +30,10 @@ class LogInfo(object):
                                                             interval=1, backupCount=1, encoding='utf-8')
             # 设置生成日志文件名的格式，以年-月-日来命名
             # suffix设置，会生成文件名为log.2020-02-25.log
-            file_hander.suffix = "%Y-%m-%d.log"
+            file_hander.suffix = "%Y-%m-%d.bak"
             # extMatch是编译好正则表达式，用于匹配日志文件名后缀
             # 需要注意的是suffix和extMatch一定要匹配的上，如果不匹配，过期日志不会被删除。
-            file_hander.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}.log$")
+            file_hander.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}.bak$")
             #往文件里写入#指定间隔时间自动生成文件的处理器
             #实例化TimedRotatingFileHandler
             #interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
