4a0a77c54f35fbd6784fa89268992af26f11b820
qiangcao <caoqianghappy@126.com>
2025-01-03 10:39:04 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -9,7 +9,7 @@ ShellDir=${MITM_DIR:-$(
 )}
 LogDir=${ShellDir}/logs
 [ ! -d ${LogDir} ] && mkdir -p ${LogDir}
-OwnActionsURL=https://gitee.com/getready/mitmproxy.git
+OwnActionsURL=https://proxy.v2gh.com/https://github.com/ronghuaxueleng/toutiao.git
 
 ## 更新Shell源码
 function Git_PullShell() {
